2137fe4bb88c205d16f14208991733f1d57c8d71
churritzu <churritzu@yahoo.com>
2024-10-07 09:59:25 -0700

    Some stuff to Neotree.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,23 +1,21 @@
 return {
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			require("neo-tree").setup({
-				window={
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			window = {
 				mappings = {
-						-- ["<cr>"] = "open_tabnew"
-					}
-				}
-			})
-			-- vim.keymap.set('n', '<Space>e', ':Neotree reveal right<CR>', { desc = 'Reveal Neotree' })
-			vim.keymap.set('n', '<Space>e', ':Neotree float reveal toggle<CR>', { desc = 'Reveal Neotree' })
-			-- vim.keymap.set('n', '<leader>p', ':Neotree buffers<CR>', { desc = 'Reveal Neotree' })
-			vim.keymap.set('n', '<leader>p', ':Neotree float reveal buffers<CR>', { desc = 'Reveal Neotree' })
-			vim.keymap.set('n', '<leader>g', ':Neotree float reveal git_status<CR>', { desc = 'Reveal Neotree' })
-		end
+					-- ["<cr>"] = "open_tabnew"
+				},
+			},
+		})
+		vim.keymap.set("n", "<Space>e", ":Neotree float reveal toggle<CR>", { desc = "Reveal Neotree" })
+		vim.keymap.set("n", "<leader>p", ":Neotree float reveal buffers<CR>", { desc = "Reveal Neotree Buffers" })
+		vim.keymap.set("n", "<leader>g", ":Neotree float reveal git_status<CR>", { desc = "Reveal Neotree Git" })
+	end,
 }
